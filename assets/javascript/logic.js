ccf82c0b1fc781e6a054a1c1b3b3f9cb6c858128
Michael Bailey <michaeljaybailey@gmail.com>
2020-05-23 18:15:10 -0700

    finished javascript functions and fixed errors

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    var config = {
+$(document).ready(function() {
+    
+    var firebaseConfig = {
         apiKey: "AIzaSyAdyu6pLlB_Vz2RxRaxauaB6kyxRgkOddU",
         authDomain: "train-scheduler-d727a.firebaseapp.com",
         databaseURL: "https://train-scheduler-d727a.firebaseio.com",
@@ -9,7 +10,9 @@ $(document).ready(function(){
         appId: "1:1031356229404:web:3a34b74fc0a41ff97fbcd9",
         measurementId: "G-NBQ4MBRYWE"
       };
-      firebase.initializeApp(config);
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
 
       var database = firebase.database();
 
@@ -36,7 +39,34 @@ $(document).ready(function(){
         var newDestination = childSnapshot.val().destinationname;
         var newFirstTrain = childSnapshot.val().firsttrain;
         var newFrequency = childSnapshot.val().frequencyinput;
-    })
+
+        var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
+
+        var currentTime = moment();
+
+        var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+
+        var tRemainder = diffTime % newFrequency;
+
+        var minutesTill = newFrequency - tRemainder;
+        
+        var nextTrain = moment().add(minutesTill, "minutes");
+        var nextTrainTime = moment(nextTrain).format("HH:mm");
+
+        $("#display-info").append(
+            ' <tr><td>' + newTrain +
+            ' </td><td>' + newDestination +
+            ' </td><td>' + newFrequency +
+            ' </td><td>' + nextTrainTime +
+            ' </td><td>' + minutesTill + ' </td><tr>');
+    
+
+    $("#train-name, #destination-name, #first-train, #frequency-input").val("");
+    return false;
+},
 
 
-})
+    function (errorObject){
+        console.log("Errors handled: " + errorObject.code);
+    });
+});
